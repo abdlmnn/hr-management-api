df10891c712258222658a401b8d25da65d0fe5c9
mda <mohammaddomangcag.abdulmanan@my.smciligan.edu.ph>
2025-12-20 10:17:51 +0800

    Add validation to prevent duplicate names.

--- job_types/serializers.py
+++ job_types/serializers.py
@@ -10,3 +10,8 @@ class JobTypeSerializer(serializers.ModelSerializer):
             "created_by",
             "date_created",
         ]
+
+    def validate_name(self, value):
+        if JobType.objects.filter(name=value).exists():
+            raise serializers.ValidationError("Job Type already exists")
+        return value
