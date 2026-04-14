2e148793469c5832c0b3417d79b556578f2819c1
Mohammad Abdulmanan <mohammadabdulmanan24@gmail.com>
2026-04-14 08:25:57 +0800

    add missing file for utils

---
+++ utils/urls.py
@@ -0,0 +1,22 @@
+"""
+Utility endpoints URLs.
+Refer to BACKGROUND_REMOVAL_DOCUMENTATION.md for endpoint details.
+"""
+
+from django.urls import path
+
+app_name = "utils"
+
+# Lazy import prevents startup failures if dependencies are missing
+try:
+    from .views import RemoveBackgroundView
+
+    urlpatterns = [
+        path(
+            "remove-background/",
+            RemoveBackgroundView.as_view(),
+            name="remove-background",
+        ),
+    ]
+except ImportError:
+    urlpatterns = []
